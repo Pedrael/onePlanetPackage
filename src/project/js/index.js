7437cd57018550d0114e2cb333b1791e91eb8a80
Pedrael <mihail_krivic@mail.ru>
2020-08-07 00:47:15 +0300

    responsivity fixes, added footer

--- src/project/js/index.js
+++ src/project/js/index.js
@@ -1,17 +1,49 @@
-console.log('index.js')
+//import anime from 'animejs/lib/anime.es.js'
 
-import { CreatePreloader } from './../../js/objects/preloader.js'
-import { CreateDroplist } from './../../js/objects/droplist.js'
+function enableHeaderScroll(header, topclass, otherclass) {
 
-// CreatePreloader({
-//   className: '.preloader-test',
-//   delay: 2000,
-//   preloaderText: [
-//     'text1', 'text2', 'text3'
-//   ],
-//   textClassName: '.preloader-text'
+  let headerItem = document.querySelector(header)
+
+  document.addEventListener('DOMContentLoaded', function() {
+    if(window.scrollY == 0) {
+      headerItem.classList.add(topclass)
+    }
+    else {
+      headerItem.classList.add(otherclass)
+    }
+  })
+  document.addEventListener('scroll', function() {
+    if(window.scrollY == 0) {
+      headerItem.classList.add(topclass)
+      headerItem.classList.remove(otherclass)
+    }
+    else {
+      headerItem.classList.add(otherclass)
+      headerItem.classList.remove(topclass)
+    }
+  })
+}
+
+enableHeaderScroll('header', 'trans', 'blue')
+
+
+
+
+//
+// anime({
+//   targets: '.dollar2',
+//   translateY: 50,
+//   duration: 3000,
+//   direction: 'alternate',
+//   loop: true,
+//   easing: 'easeOutQuint'
 // })
-
-CreateDroplist({
-  className: '.mihail-droplist'
-})
+//
+// anime({
+//   targets: '.dollar3',
+//   translateY: 50,
+//   duration: 7000,
+//   direction: 'alternate',
+//   loop: true,
+//   easing: 'easeOutQuint'
+// })
